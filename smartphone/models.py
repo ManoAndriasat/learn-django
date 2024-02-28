ac1f08e5a9da359853e95cbc5a1db962cacedd02
Mano Andriasat <ma.andriasat@gmail.com>
2024-02-28 17:42:05 +0300

    Update models.py

--- smartphone/models.py
+++ smartphone/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from .models import Operateur
 
-class smartphone(models.Model):
+class Smartphone(models.Model):
     id = models.IntegerField(primary_key=True)
-    id_operateur = models.ForeignKey(operateur, on_delete=models.CASCADE)
+    id_operateur = models.ForeignKey(Operateur, on_delete=models.CASCADE)
     nom = models.CharField(max_length=100)
 
     def __str__(self):
